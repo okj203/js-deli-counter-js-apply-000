4ba7624cc356768fb7a907dd038ccd88147edafc
Ock Kyung Jung <okj203@gmail.com>
2020-04-16 20:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let nowServing = katzDeliLine => {
   if (katzDeliLine.length !== 0) {
     return `Currently serving ${katzDeliLine[0]}`;
   katzDeliLine.shift();
-    return `There is nobody waiting to be served!`
+    
   } else {
     
   }
