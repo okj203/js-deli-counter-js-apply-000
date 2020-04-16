9df6d473fa84fcda86e04c2dec210d5b241fe9a8
Ock Kyung Jung <okj203@gmail.com>
2020-04-16 20:33:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let takeANumber = (katzDeliLine, newPerson) => {
 
 let nowServing = katzDeliLine => {
   if (katzDeliLine.length === 0) {
-    return ``
+    return `There is nobody waiting to be served!`
+  } else {
+    
   }
   return `Currently serving ${katzDeliLine[0]}`;
   katzDeliLine.shift();
