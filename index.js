69ffea28df3ed927682e0b94b91b07764a4888d2
Ock Kyung Jung <okj203@gmail.com>
2020-04-16 20:55:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ let currentLine = katzDeliLine => {
     for (let i = 0; i < katzDeliLine.length - 1; i++) {
       let person = katzDeliLine[i];
       newStr += 'The line is currently: ' + toString(i + 1) + '. ' + person + ','
-      newStr += toString(katzDeliLine.length) + '. ' + katzDeliLine[]
+      newStr += toString(katzDeliLine.length) + '. ' + katzDeliLine[katzDeliLine.length - 1];
     }
   }
+  return newStr;
 }
 
