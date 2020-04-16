5f2b822cd8c516604b183a2cb52621bb4aeb24b6
Ock Kyung Jung <okj203@gmail.com>
2020-04-16 21:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let currentLine = katzDeliLine => {
       newLine += (i + 1) + '. ' + katzDeliLine[i] + ', '
      
     }
-    newLine = newLine.slice(0, -1);
+    // newLine = newLine.slice(0);
     return `The line is currently: ${newLine}`;
   }
 }
